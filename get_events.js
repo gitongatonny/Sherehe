00287f0953c12e5c2f0397bdfba986ae2cee7906
Tonny Gitonga <tonnygitonga04@gmail.com>
2023-04-07 02:16:20 +0300

    fixed description issues in events page

--- get_events.js
+++ get_events.js
@@ -1,18 +1,22 @@
-// Fetch events data from get_events.php
 fetch("get_events.php")
     .then(response => response.json())
     .then(data => {
         // Loop through the data and create HTML for each event
         let html = "";
         data.forEach(event => {
+            // Split the amenities string into an array of individual amenities
+            const amenities = event.amenities.split("\n");
+            // Create a new paragraph element for each amenity
+            const amenitiesHtml = amenities.map(amenity => `<p>${amenity}</p>`).join("");
+            // Build the HTML for the event card
             html += `
                 <div class="card">
                     <img src="${event.image}" alt="${event.venue}">
                     <h2>${event.venue}</h2>
-                    <p>Rating: ${event.rating}</p>
-                    <p>Capacity: ${event.capacity} guests</p>
-                    <p>Price: KSHS ${event.price}</p>
-                    <p>Amenities: ${event.amenities}</p>
+                    <p><b>Rating:</b> ${event.rating}</p>
+                    <p><b>Capacity:</b> ${event.capacity} guests</p>
+                    <p><b>Price:</b> KSHS ${event.price}</p>
+                    <div class="amenities">${amenitiesHtml}</div>
                     <button onclick="bookVenue(${event.id})">Book</button>
                 </div>
             `;
--- planning.js
+++ planning.js
@@ -1,18 +1,22 @@
-// Fetch events data from get_events.php
 fetch("get_events.php")
     .then((response) => response.json())
     .then((data) => {
         // Loop through the data and create HTML for each event
         let html = "";
         data.forEach((event) => {
+            // Split the amenities string into an array of individual amenities
+            const amenities = event.amenities.split("\n");
+            // Create a new paragraph element for each amenity
+            const amenitiesHtml = amenities.map(amenity => `<p>${amenity}</p>`).join("");
+            // Build the HTML for the event card
             html += `
                 <div class="card">
                     <img src="${event.image}" alt="${event.venue}">
                     <h2>${event.venue}</h2>
-                    <p>Rating: ${event.rating}</p>
-                    <p>Capacity: ${event.capacity} guests</p>
-                    <p>Amenities: ${amenities}</p>
-                    <p>Price: KSHS ${event.price}</p>
+                    <p><strong>Rating:</strong> ${event.rating}</p>
+                    <p><strong>Capacity:</strong> ${event.capacity} guests</p>
+                    <div class="amenities">${amenitiesHtml}</div>
+                    <p><strong>Price:</strong> KSHS ${event.price}</p>
                     <button onclick="bookVenue(${event.id})">Book</button>
                 </div>
             `;
@@ -23,39 +27,3 @@ fetch("get_events.php")
         cardContainer.innerHTML = html;
     })
     .catch((error) => console.error(error));
-
-
-// Function to handle booking a venue
-function bookVenue(venueId) {
-    // Send an AJAX request to the server to retrieve the venue information
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const venue = JSON.parse(this.responseText);
-            const { venueName, capacity, price } = venue;
-            // Redirect to the check-out page with the venue information as parameters
-            window.location.href = `check-out.php?id=${venueId}&name=${venueName}&capacity=${capacity}&price=${price}`;
-        }
-    };
-    xhttp.open("GET", `get-venue.php?id=${venueId}`, true);
-    xhttp.send();
-}
-
-// Function to handle hover effect on cards
-function cardHoverEffect() {
-    const cards = document.querySelectorAll(".card");
-    cards.forEach((card) => {
-        card.addEventListener("mouseover", () => {
-            card.style.boxShadow = "2px 2px 10px rgba(0, 0, 0, 0.3)";
-            card.style.transform = "translateY(-10px)";
-            card.style.transition = "all 0.3s ease-in-out";
-        });
-        card.addEventListener("mouseout", () => {
-            card.style.boxShadow = "none";
-            card.style.transform = "none";
-        });
-    });
-}
-
-// Call the cardHoverEffect function on window load
-window.addEventListener("load", cardHoverEffect);
