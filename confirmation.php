26f93da481dafc9eb91cfbc6d826c26454393ad4
geek0254 <etyangm98@gmail.com>
2023-04-10 12:14:18 +0300

    Made the following changes and modifications:
    1. Checkout table event type Included
    2. Added the event type too in the db
    3. Retrieval of the event type in the profile dash
    4. Customized the invitation to retrieve date, event type and username
    5. Fixed the Edit button in admin
    6. Styling the invitation card.
    7. Fixed the admin dash to redirect back to the dash when on the logo

--- confirmation.php
+++ confirmation.php
@@ -26,23 +26,76 @@
         <h3>Success!</h3>
         <p>Your booking has been confirmed.</p>
 
-        <!-- Generate invitation card here -->
-        <button id="download-button" onclick="generateInvitationCard()">Download Invitation Card</button>
+        <?php
+        // Check if user is logged in
+        session_start();
+        if (isset($_SESSION['username'])) {
+            // Connect to database
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "sherehe";
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            // Check connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            // Retrieve event and date from checkout table
+            $sql = "SELECT events, date FROM checkout";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                // Output data of each row
+                while ($row = mysqli_fetch_assoc($result)) {
+                    // Generate invitation card
+                    echo '<button id="download-button" onclick="generateInvitationCard(\'' . $_SESSION['username'] . '\', \'' . $row['events'] . '\', \'' . $row['date'] . '\')">Download Invitation Card</button>';
+                }
+            } else {
+                echo "0 results";
+            }
+
+            //Retrieve venue that was booked from venues table using the bookVenue(${event.id}) of the Book button
+            
+
+
+            mysqli_close($conn);
+        } else {
+            // User not logged in, display error message
+            echo '<p>You must be logged in to download the invitation card.</p>';
+        }
+        ?>
+
         <br>
         <button id="download-button"><a href="profile.php">Go To Your Profile</a></button>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
         <script>
-            function generateInvitationCard() {
+            function generateInvitationCard(username, events, date) {
                 // Create new jsPDF object
                 var pdf = new jsPDF();
 
                 // Set font
                 pdf.setFont('helvetica', 'bold');
-                pdf.setFontSize(20);
+                pdf.setFontSize(16);
+
+                // Set styles
+                const cardWidth = 180;
+                const cardHeight = 120;
+                const borderRadius = 10;
+                const backgroundColor = '#f2f2f2';
+                const borderColor = '#333';
+                const padding = 10;
+
+                // Draw card background
+                pdf.setFillColor(backgroundColor);
+                pdf.setDrawColor(borderColor);
+                pdf.roundedRect(15, 20, cardWidth, cardHeight, borderRadius, borderRadius, 'FD');
 
                 // Write invitation text
-                pdf.text('You are cordially invited to our party!', 15, 20);
+                pdf.setTextColor('#333');
+                pdf.text(`Dear ${username},\n\nYou are cordially invited to host the following event:\n\n- ${events}\n\n- Date: ${date}\n\nPlease RSVP by responding to this email(admin@sherehe.com)\n\n or contacting the event organizer directly.\n\nWe look forward to seeing you there!`, 25, 40);
 
                 // Save PDF file
                 pdf.save('invitation_card.pdf');
